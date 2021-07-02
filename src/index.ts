7e90ae41c8878bf305a64422da52a1eb813661ba
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-07-02 16:54:31 -0300

    criação e leitura de música

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import app from "./app"
+import createMusic from "./endpoints/music/createMusic"
+import getMusic from "./endpoints/music/getMusic"
 import login from "./endpoints/user/login"
 import signUp from './endpoints/user/signUp'
+import getMusicById from './endpoints/music/getMusicById'
 
-app.post('/users/signup', signUp)
+app.get('/music/all', getMusic)
+app.get('/music/:id', getMusicById)
+app.post('/users/signUp', signUp)
 app.post('/users/login', login)
+app.post('/music', createMusic)
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,6 +1,11 @@
 export type user = {
+	id: string,
     name: string,
 	email: string,
 	nickname: string,
 	password: string
+}
+
+export type authenticationData = {
+	id: string
 }
